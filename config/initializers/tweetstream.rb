bf9549e37bb4119cb69d1c555927944350b50367
elzzup <guild0105@gmail.com>
2015-09-17 19:47:56 +0900

    add debugger

--- config/initializers/tweetstream.rb
+++ config/initializers/tweetstream.rb
@@ -1,4 +1,7 @@
 twitter_conf = Rails.application.secrets.twitter_app
+if twitter_conf['consumer_key'].nil?
+  puts 'error: empty env values, twitter auth keys'
+end
 TweetStream.configure do |config|
   config.consumer_key       = twitter_conf['consumer_key']
   config.consumer_secret    = twitter_conf['consumer_secret']
